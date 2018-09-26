53778956ecb356c1f33c40a6c8a557d9ede24d61
reeechart <ferdinandusrichard@yahoo.co.id>
2018-09-27 00:59:20 +0700

    add status message assignment in response ctor

--- src/http/response.cpp
+++ src/http/response.cpp
@@ -3,18 +3,18 @@
 #include <sstream>
 using namespace std;
 
-HTTPResponse::HTTPResponse(int status, string status_message,  string body) {
+HTTPResponse::HTTPResponse(int status, string status_message, string body) {
     this->status = status;
-    this->body = body; 
+    this->status_message = status_message;
+    this->body = body;
 }
-
 
 string HTTPResponse::ToString() {
     ostringstream ss;
     ss << "HTTP/1.1 " << this->status << " " << this->status_message << endl;
     ss << "Content-Type: text/html" << endl;
     ss << "Content-Length: " << this->body.length() << endl;
-    ss << "Connection: close" << endl;
+    ss << "Connection: Closed" << endl;
     ss << endl;
     ss << this->body << endl;
     return ss.str();
